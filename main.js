af39f30774cfe2725d350ea0e8f6172d6dc1d206
caiogn <caiocesar.gn@gmail.com>
2024-03-28 16:10:04 -0300

    lampada ligada e desligada

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const lamp = document.querySelector("#on img")
+const button = document.querySelector("#apertar")
+const info = document.querySelector("#info h1")
+
+let lampadaAcesa = true; 
+
+button.addEventListener('click', function() {
+    if(lampadaAcesa) {
+        lamp.src= "./img/desligada.jpg";
+        button.textContent = "LIGAR";
+        info.textContent = "Lampada Apagada";
+    }
+    else {
+        lamp.src = "./img/ligada.jpg";
+        button.textContent = "DESLIGAR";
+        info.textContent = "Lampada Acesa";
+    }
+    lampadaAcesa = !lampadaAcesa;
+});
+});
